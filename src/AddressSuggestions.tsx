cce64026147bd1a322ae4685cd82641d640dd16c
vakkerrv <77551843+vakkerrv@users.noreply.github.com>
2021-08-11 18:23:58 +0100

    restricted_value option added

--- src/AddressSuggestions.tsx
+++ src/AddressSuggestions.tsx
@@ -17,7 +17,7 @@ export class AddressSuggestions extends BaseSuggestions<DaDataAddress, Props> {
   loadSuggestionsUrl = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address';
 
   getLoadSuggestionsData = () => {
-    const { count, filterFromBound, filterToBound, filterLocations, filterLocationsBoost, filterLanguage } = this.props;
+    const { count, filterFromBound, filterToBound, filterLocations, filterLocationsBoost, filterLanguage, restrictValue } = this.props;
     const { query } = this.state;
 
     const requestPayload: any = {
@@ -46,6 +46,10 @@ export class AddressSuggestions extends BaseSuggestions<DaDataAddress, Props> {
       requestPayload.locations_boost = filterLocationsBoost;
     }
 
+    if (restrictValue){
+        requestPayload.restrict_value = true;
+    }
+    
     return requestPayload;
   }
 
